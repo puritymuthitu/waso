69610d6ce1520f1726aa8b7b22622a5528716a1b
puritymuthitu <97535851+puritymuthitu@users.noreply.github.com>
2022-04-29 12:15:11 -0400

    done

---
+++ contactus.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    $('.submit').click(function (event){
+      event.preventDefault()
+      console.log('Clicked button')
+  
+      var email = $('.email').val()
+      var subject = $('.subject').val()
+      var message = $('.message').val()
+      var statusElm = $('.stutus')
+  
+  
+      
+  
+    })
+  })
---
+++ photogallery.js
@@ -0,0 +1,7 @@
+var fullImgBox = document.getElementById(fullImgBox);
+var fullImg = document.getElementById(fullImg);
+
+function openFullImg(pic){
+    fullImgBox.style.display = "flex";
+    fullImg.src= pic;
+}
