3e74ea0f95a4ef675701ffde215e0b858d2dd8d4
puritymuthitu <97535851+puritymuthitu@users.noreply.github.com>
2022-05-03 13:50:49 -0400

    done

--- contactus.js
+++ contactus.js
@@ -1,15 +1,31 @@
 $(document).ready(function () {
     $('.submit').click(function (event){
       event.preventDefault()
-      console.log('Clicked button')
-  
-      var email = $('.email').val()
-      var subject = $('.subject').val()
-      var message = $('.message').val()
-      var statusElm = $('.stutus')
-  
-  
+      var toSubmit = {};
+      toSubmit.email = $("#email").val();
+      toSubmit.name = $("#name").val();
+      toSubmit.phone = $("#phone").val();
+      toSubmit.message = $("#message").val();
+      $.ajaxSetup({
+        headers: {
+          'Accept': "application/json",
+        }
+      });
       
-  
+      var form_submit = $.post("https://formspree.io/f/mqkwawqv", toSubmit);
+      
+      form_submit.done(function(){
+        $("#form-success").modal('show');
+        $("#email").val("");
+        $("#name").val("");
+        $("phone").val("");
+        $("#message").val("");
+      });
+      
+      form_submit.fail(function(){
+        $("#form-error").modal('show');
+      });
+      
+      
     })
-  })
+  });
